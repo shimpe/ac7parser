4485ac9e4679e369c1dda48fbd307f1a0eb516c0
stefaan himpe <stefaan.himpe@gmail.com>
2019-10-07 23:38:20 +0200

    add a setup.py file

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='ac7parser',
+    version='0.0.1',
+    packages=['ac7parser'],
+    url='https://github.com/shimpe/ac7parser',
+    license='GPLv3',
+    author='shiihs',
+    author_email='b13d1be5@opayq.com',
+    description='parser for ac7 files'
+)
