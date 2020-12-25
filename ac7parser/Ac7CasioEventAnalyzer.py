33949a0b812646acbc607d461c55b7b08cea71ef
stefaan himpe <stefaan.himpe@gmail.com>
2020-12-25 23:52:01 +0100

    add more annotations

--- ac7parser/Ac7CasioEventAnalyzer.py
+++ ac7parser/Ac7CasioEventAnalyzer.py
@@ -45,10 +45,6 @@ class Ac7CasioEventAnalyzer(object):
             casioevent['annotation'] = 'mod wheel off'
         if casioevent['note_or_event'] == 229:
             casioevent['annotation'] = 'start of user recorded data'
-        if casioevent['note_or_event'] == 227:
-            casioevent['annotation'] = 'UNKNOWN'
-        if casioevent['note_or_event'] == 228:
-            casioevent['annotation'] = 'UNKNOWN - prevent recording/editing?'
         if casioevent['note_or_event'] == 255 and casioevent['vel_or_val'] != 0:
             casioevent['annotation'] = 'longdelta doesn\'t fit in single BYTE; longdelta = (vel_or_val << 8) + delta'
         if casioevent['note_or_event'] == 255 and casioevent['vel_or_val'] == 0 and casioevent['delta'] == 0:
@@ -56,12 +52,47 @@ class Ac7CasioEventAnalyzer(object):
         if casioevent['note_or_event'] == 181:
             casioevent['annotation'] = 'modulation wheel'
         if casioevent['note_or_event'] == 186:
-            casioevent['annotation'] = 'cut-off'
+            casioevent['annotation'] = 'filter cut-off'
         if casioevent['note_or_event'] == 187:
-            casioevent['annotation'] = 'harmonics'
+            casioevent['annotation'] = 'filter resonance'
         if casioevent['note_or_event'] == 185:
             casioevent['annotation'] = 'pitch bend range'
         if casioevent['note_or_event'] == 142:
             casioevent['annotation'] = 'pitch bend value {0}'.format(casioevent['vel_or_val'])
         if casioevent['note_or_event'] == 252:
             casioevent['annotation'] = 'end of part; longdelta = (vel_or_val << 8) + delta'
+        # speculative
+        if casioevent['note_or_event'] == 135:
+            casioevent['annotation'] = 'Expression LSB'
+        if casioevent['note_or_event'] == 141:
+            casioevent['annotation'] = 'after touch'
+        if casioevent['note_or_event'] == 160:
+            casioevent['annotation'] = "DANGER! Triggers SW failure. Don't use!"
+        if casioevent['note_or_event'] == 169:
+            casioevent['annotation'] = "Reset all parameters to default values"
+        if casioevent['note_or_event'] == 177:
+            casioevent['annotation'] = "Hold/sustain pedal"
+        if casioevent['note_or_event'] == 178:
+            casioevent['annotation'] = "Soft pedal"
+        if casioevent['note_or_event'] == 179:
+            casioevent['annotation'] = "Sostenuto pedal"
+        if casioevent['note_or_event'] == 188:
+            casioevent['annotation'] = "attack time"
+        if casioevent['note_or_event'] == 189:
+            casioevent['annotation'] = "release time"
+        if casioevent['note_or_event'] == 224:
+            casioevent['annotation'] = "chord conversion table 0-15"
+        if casioevent['note_or_event'] == 225:
+            casioevent['annotation'] = "chord inversion (0=off;1=on;2=7th;3=undocumented setting for forcing 7th"
+        if casioevent['note_or_event'] == 226:
+            casioevent['annotation'] = "retrigger (2=off; other=on)"
+        if casioevent['note_or_event'] == 227:
+            casioevent['annotation'] = "tempo increase (add value to original tempo in bpm, to max of 255"
+        if casioevent['note_or_event'] == 228:
+            casioevent['annotation'] = "tempo slowdown (subtract value from original tempo to minimum of 20"
+        if casioevent['note_or_event'] == 230:
+            casioevent['annotation'] = "highest note after chord conversion (0-127)"
+        if casioevent['note_or_event'] == 231:
+            casioevent['annotation'] = "UNKNOWN"
+        if casioevent['note_or_event'] == 233:
+            casioevent['annotation'] = "(Casio - specific MIDI CC 0x59)"
